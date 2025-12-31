ec7e3679295156947ee1b6bcbf9e50f0c4d0ea9f
Robbie McArthur <robbiemca123@gmail.com>
2025-12-31 11:26:05 +0000

    initial bootstrap project

---
+++ src/main/java/org/robbiemcarthur/creatorledger/CreatorLedgerApplication.java
@@ -0,0 +1,13 @@
+package org.robbiemcarthur.creatorledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorLedgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatorLedgerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/robbiemcarthur/creatorledger/CreatorLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.robbiemcarthur.creatorledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorLedgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
